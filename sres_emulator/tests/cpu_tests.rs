8f25e27157357468a48411df117be12343066a82
Dennis Kempin <dennis.kempin@gmail.com>
2023-05-11 20:07:57 -0700

    Add remaining KROM tests
    Gathered trace logs using BSNES+ V05, trimmed them to just the range
    until the infinite loop.

--- sres_emulator/tests/cpu_tests.rs
+++ sres_emulator/tests/cpu_tests.rs
@@ -22,18 +22,120 @@ pub fn test_cpuasl() {
 }
 
 #[test]
-pub fn test_cpubra() {
-    run_krom_test("CPUBRA");
-}
-
-#[test]
 pub fn test_cpubit() {
     run_krom_test("CPUBIT");
 }
 
 #[test]
+pub fn test_cpubra() {
+    run_krom_test("CPUBRA");
+}
+
+#[test]
 pub fn test_cpucmp() {
     run_krom_test("CPUCMP");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpudec() {
+    run_krom_test("CPUDEC");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpueor() {
+    run_krom_test("CPUEOR");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpuinc() {
+    run_krom_test("CPUINC");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpujmp() {
+    run_krom_test("CPUJMP");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpuldr() {
+    run_krom_test("CPULDR");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpulsr() {
+    run_krom_test("CPULSR");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpumov() {
+    run_krom_test("CPUMOV");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpumsc() {
+    run_krom_test("CPUMSC");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpuora() {
+    run_krom_test("CPUORA");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpuphl() {
+    run_krom_test("CPUPHL");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpupsr() {
+    run_krom_test("CPUPSR");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpuret() {
+    run_krom_test("CPURET");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpurol() {
+    run_krom_test("CPUROL");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpuror() {
+    run_krom_test("CPUROR");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpusbc() {
+    run_krom_test("CPUSBC");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cpustr() {
+    run_krom_test("CPUSTR");
+}
+
+#[test]
+#[ignore = "Instructions not implemented yet"]
+pub fn test_cputrn() {
+    run_krom_test("CPUTRN");
 }
 
 fn run_krom_test(test_name: &str) {
