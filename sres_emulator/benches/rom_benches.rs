40658c7f852b630a9beb538c03bfd7d64c0d8817
Dennis Kempin <dennis.kempin@gmail.com>
2023-09-14 16:57:54 -0700

    Fix benches
    Current result is around 5ms per frame. That's.. uncomfortably close
    to 16ms considering the limited functionality so far.

--- sres_emulator/benches/rom_benches.rs
+++ sres_emulator/benches/rom_benches.rs
@@ -8,7 +8,7 @@ use sres_emulator::System;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let rom_path = root_dir.join("tests/rom_tests/krom_adc.sfc");
+    let rom_path = root_dir.join("tests/rom_tests/krom_tadc.sfc");
     c.bench_function("krom_frame_time", |b| {
         b.iter_batched_ref(
             || System::with_sfc(&rom_path).unwrap(),
