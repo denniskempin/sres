7804dca5bdb1d05f7f8096300884295f7224b672
Dennis Kempin <dennis.kempin@gmail.com>
2025-03-01 11:58:50 -0800

    cpu status: Do not use packed struct
    this allows us to manually implement a Display trait
    that renders nicely in the debug UI

--- sres_emulator/src/components/cpu/status.rs
+++ sres_emulator/src/components/cpu/status.rs
@@ -1,8 +1,7 @@
 //! Type for status register and boilerplate for conversion and display
-use packed_struct::prelude::*;
+use intbits::Bits;
 
-#[derive(PackedStruct, Clone, Debug, Copy, PartialEq, Eq)]
-#[packed_struct(bit_numbering = "msb0")]
+#[derive(Clone, Debug, Copy, PartialEq, Eq)]
 pub struct StatusFlags {
     pub negative: bool,
     pub overflow: bool,
@@ -29,15 +28,56 @@ impl Default for StatusFlags {
     }
 }
 
+impl std::fmt::Display for StatusFlags {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}{}{}{}{}{}{}",
+            if self.negative { "N" } else { "-" },
+            if self.overflow { "V" } else { "-" },
+            if self.accumulator_register_size {
+                "M"
+            } else {
+                "-"
+            },
+            if self.index_register_size_or_break {
+                "X"
+            } else {
+                "-"
+            },
+            if self.decimal { "D" } else { "-" },
+            if self.irq_disable { "I" } else { "-" },
+            if self.zero { "Z" } else { "-" },
+            if self.carry { "C" } else { "-" }
+        )
+    }
+}
+
 // Shorthand to convert StatusFlags into and from u8 reqister value
 impl From<u8> for StatusFlags {
     fn from(value: u8) -> Self {
-        StatusFlags::unpack(&[value]).unwrap()
+        Self {
+            negative: value.bit(7),
+            overflow: value.bit(6),
+            accumulator_register_size: value.bit(5),
+            index_register_size_or_break: value.bit(4),
+            decimal: value.bit(3),
+            irq_disable: value.bit(2),
+            zero: value.bit(1),
+            carry: value.bit(0),
+        }
     }
 }
 
 impl From<StatusFlags> for u8 {
     fn from(value: StatusFlags) -> Self {
-        value.pack().unwrap()[0]
+        0u8.with_bit(7, value.negative)
+            .with_bit(6, value.overflow)
+            .with_bit(5, value.accumulator_register_size)
+            .with_bit(4, value.index_register_size_or_break)
+            .with_bit(3, value.decimal)
+            .with_bit(2, value.irq_disable)
+            .with_bit(1, value.zero)
+            .with_bit(0, value.carry)
     }
 }
