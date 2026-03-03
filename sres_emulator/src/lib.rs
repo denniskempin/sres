defe5588b5b260bade19d60f2e3f4423e82a34e3
Dennis Kempin <dennis.kempin@gmail.com>
2026-03-02 20:06:34 -0800

    Refactor BusDevice wrappers
    In order to make it easier to switch between wrapper implementations,
    introduce a ManagedBusDeviceU24 wrapper. This allows us to use
    the fully sync version in testing.
    
    In order to also support the async version, more changes are needed
    to make the Apu async capable

--- sres_emulator/src/lib.rs
+++ sres_emulator/src/lib.rs
@@ -9,6 +9,8 @@ use std::cell::RefMut;
 use std::ops::Deref;
 
 use common::bus::BatchedBusDeviceU24;
+use common::bus::ManagedBusDeviceU24;
+use common::bus::SyncBusDevice;
 use common::util::EdgeDetector;
 use components::ppu::Framebuffer;
 use components::ppu::PpuDebug;
@@ -33,10 +35,20 @@ pub enum ExecutionResult {
     Halt,
     Break(BreakReason),
 }
+
 pub type CpuT = Cpu<MainBusImpl<BatchedBusDeviceU24<Ppu>, BatchedBusDeviceU24<Apu>>>;
 
-pub struct System {
-    pub cpu: CpuT,
+/// System using batched PPU and APU updates for performance.
+pub type BatchedSystem = SystemImpl<BatchedBusDeviceU24<Ppu>, BatchedBusDeviceU24<Apu>>;
+
+/// System using synchronous PPU and APU updates on every cycle.
+pub type SyncSystem = SystemImpl<SyncBusDevice<Ppu>, SyncBusDevice<Apu>>;
+
+/// Default implementation used in UI
+pub type System = BatchedSystem;
+
+pub struct SystemImpl<PpuT: ManagedBusDeviceU24<Ppu>, ApuT: ManagedBusDeviceU24<Apu>> {
+    pub cpu: Cpu<MainBusImpl<PpuT, ApuT>>,
     debugger: DebuggerRef,
     debugger_enabled: bool,
     vblank_detector: EdgeDetector,
@@ -44,7 +56,20 @@ pub struct System {
     pending_video_frame: Framebuffer,
 }
 
-impl System {
+impl<PpuT: ManagedBusDeviceU24<Ppu>, ApuT: ManagedBusDeviceU24<Apu>> SystemImpl<PpuT, ApuT> {
+    pub fn with_cpu(cpu: Cpu<MainBusImpl<PpuT, ApuT>>, debugger: DebuggerRef) -> Self {
+        SystemImpl {
+            cpu,
+            debugger,
+            debugger_enabled: false,
+            vblank_detector: EdgeDetector::new(),
+            has_pending_video_frame: false,
+            pending_video_frame: Framebuffer::default(),
+        }
+    }
+}
+
+impl BatchedSystem {
     #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         Self::with_cartridge(&Cartridge::default())
@@ -52,8 +77,8 @@ impl System {
 
     pub fn with_cartridge(cartridge: &Cartridge) -> Self {
         let debugger = Debugger::new();
-        Self {
-            cpu: Cpu::new(
+        Self::with_cpu(
+            Cpu::new(
                 MainBusImpl::new(
                     cartridge,
                     BatchedBusDeviceU24::new(Ppu::new()),
@@ -63,16 +88,37 @@ impl System {
                 DebugEventCollectorRef(debugger.clone()),
             ),
             debugger,
-            debugger_enabled: false,
-            vblank_detector: EdgeDetector::new(),
-            has_pending_video_frame: false,
-            pending_video_frame: Framebuffer::default(),
-        }
+        )
+    }
+}
+
+impl SyncSystem {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Self::with_cartridge(&Cartridge::default())
     }
 
+    pub fn with_cartridge(cartridge: &Cartridge) -> Self {
+        let debugger = Debugger::new();
+        Self::with_cpu(
+            Cpu::new(
+                MainBusImpl::new(
+                    cartridge,
+                    SyncBusDevice::new(Ppu::new()),
+                    SyncBusDevice::new(Apu::new(debugger.clone())),
+                    debugger.clone(),
+                ),
+                DebugEventCollectorRef(debugger.clone()),
+            ),
+            debugger,
+        )
+    }
+}
+
+impl<PpuT: ManagedBusDeviceU24<Ppu>, ApuT: ManagedBusDeviceU24<Apu>> SystemImpl<PpuT, ApuT> {
     fn execute_until<F>(&mut self, should_break: F) -> ExecutionResult
     where
-        F: Fn(&CpuT) -> bool,
+        F: Fn(&Cpu<MainBusImpl<PpuT, ApuT>>) -> bool,
     {
         loop {
             if self.cpu.halted() {
@@ -117,8 +163,8 @@ impl System {
     }
 
     pub fn execute_for_audio_samples(&mut self, count: usize) -> ExecutionResult {
-        let target_sample_count = self.cpu.bus.apu.inner.sample_buffer_size() + count;
-        self.execute_until(|cpu| cpu.bus.apu.inner.sample_buffer_size() >= target_sample_count)
+        let target_sample_count = self.cpu.bus.apu.inner().sample_buffer_size() + count;
+        self.execute_until(|cpu| cpu.bus.apu.inner().sample_buffer_size() >= target_sample_count)
     }
 
     pub fn execute_for_duration(&mut self, seconds: f64) -> ExecutionResult {
@@ -146,15 +192,15 @@ impl System {
     }
 
     pub fn swap_audio_buffer(&mut self, buffer: &mut AudioBuffer) {
-        self.cpu.bus.apu.inner.swap_audio_buffer(buffer)
+        self.cpu.bus.apu.inner_mut().swap_audio_buffer(buffer)
     }
 
-    pub fn ppu(&mut self) -> &mut Ppu {
-        &mut self.cpu.bus.ppu.inner
+    pub fn force_headless(&mut self) {
+        self.cpu.bus.ppu.inner_mut().force_headless();
     }
 
-    pub fn apu(&mut self) -> &mut Apu {
-        &mut self.cpu.bus.apu.inner
+    pub fn save_ppu_state(&self) -> Vec<u8> {
+        self.cpu.bus.ppu.inner().save_state()
     }
 
     pub fn debug_until(&mut self, event: EventFilter) -> ExecutionResult {
@@ -196,17 +242,17 @@ impl System {
             self.cpu
                 .bus
                 .ppu
-                .inner
+                .inner_mut()
                 .swap_framebuffer(&mut self.pending_video_frame);
             self.has_pending_video_frame = true;
         }
     }
 
     /// Exposes debug information for investigating the system state.
-    pub fn debug(&self) -> SystemDebug<'_> {
+    pub fn debug(&self) -> SystemDebug<'_, PpuT, ApuT> {
         SystemDebug {
-            ppu: &self.cpu.bus.ppu.inner,
-            apu: &self.cpu.bus.apu.inner,
+            ppu: self.cpu.bus.ppu.inner(),
+            apu: self.cpu.bus.apu.inner(),
         }
     }
 
@@ -216,12 +262,15 @@ impl System {
     }
 }
 
-pub struct SystemDebug<'a> {
-    ppu: &'a Ppu,
-    apu: &'a Apu,
+pub struct SystemDebug<'a, PpuT: ManagedBusDeviceU24<Ppu> + 'a, ApuT: ManagedBusDeviceU24<Apu> + 'a>
+{
+    ppu: PpuT::InnerRef<'a>,
+    apu: ApuT::InnerRef<'a>,
 }
 
-impl<'a> SystemDebug<'a> {
+impl<'a, PpuT: ManagedBusDeviceU24<Ppu> + 'a, ApuT: ManagedBusDeviceU24<Apu> + 'a>
+    SystemDebug<'a, PpuT, ApuT>
+{
     pub fn ppu(&'a self) -> PpuDebug<'a> {
         self.ppu.debug()
     }
